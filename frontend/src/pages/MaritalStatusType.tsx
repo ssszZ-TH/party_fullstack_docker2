02f595f9ab0dfaceb05d34c2366ce9df63d20c09
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2025-03-26 16:22:33 +0700

    frontend person name type fin tested

--- frontend/src/pages/MaritalStatusType.tsx
+++ frontend/src/pages/MaritalStatusType.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import AppBarCustom from "../components/AppBarCustom";
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../components/DataTable";
-import MaritalStatusTypeModal from "../components/MaritalstatustypeModal";
+import MaritalStatusTypeModal from "../components/Modal_id_des";
 import { Button } from "@mui/material";
 import Loading from "../components/Loading"; // สมมุติว่ามีคอมโพเนนต์สำหรับแสดงสถานะการโหลด
 import {
