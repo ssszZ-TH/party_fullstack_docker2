f5a6b0aad701330a462e5a13933fe757416a8817
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2025-03-26 11:02:14 +0700

    frontend basic structure fin

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, Suspense, lazy } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import('./pages/Home'));
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          {/* สามารถเพิ่ม Route อื่น ๆ ได้ที่นี่ */}
+          
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  </StrictMode>
 )
